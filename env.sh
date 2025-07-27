02224f5b166ff4e9b6935cc24f1b8fabaad254de
Chris Gresty <chris@gresty.dev>
2025-07-27 12:49:32 +0100

    Update to version 2.10.0

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export IMAGE=caddy
-export IMAGE_VERSION=2.8.4
+export IMAGE_VERSION=2.10.0
 export SUFFIX=2tudor
 export REGISTRY=docker.2tudor.net
 export CA_ROOT=root_ca-2tudor.net.crt
